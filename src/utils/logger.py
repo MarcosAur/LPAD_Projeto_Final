fddc214fbb45fe0b54e53fd5b1e7d05151cc90d2
Evellen <111620613+EvellenGomes@users.noreply.github.com>
2025-06-20 08:28:14 -0300

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -7,7 +7,15 @@ class Logger:
         with open("../logs/execucao.log", "a") as file:
             now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
             file.write(f"[{type}] - {now} - {message}\n")
-
+            
+ #def __write_log(self, type: str, message: str):
+        #log_dir = os.path.join(os.path.dirname(__file__), '../../logs')
+        #os.makedirs(log_dir, exist_ok=True)
+        #log_path = os.path.join(log_dir, 'execucao.log')
+        #with open(log_path, "a", encoding="utf-8") as file:
+            #now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            #file.write(f"[{type}] - {now} - {message}\n")
+            
     def info(self, message: str) -> None:
         self.__write_log("INFO", message)
 
@@ -16,3 +24,4 @@ class Logger:
 
     def warning(self, message: str) -> None:
         self.__write_log("WARNING", message)
+
