74ff03d08edab0a843221acd93b6468c9f77f24d
Evellen <111620613+EvellenGomes@users.noreply.github.com>
2025-06-20 08:16:05 -0300

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,5 +1,12 @@
-from src.utils.logger import Logger
-
-
-class Pipeline(Logger):
-    pass
+class Pipeline:
+    def __init__(self):
+        self.tasks = []
+    
+    def add_task(self, task):
+        """Adiciona uma tarefa à pipeline"""
+        self.tasks.append(task)
+    
+    def execute(self):
+        """Executa todas as tarefas na ordem em que foram adicionadas"""
+        for task in self.tasks:
+            task.execute() 
